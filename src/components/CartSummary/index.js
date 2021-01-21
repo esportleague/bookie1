416c7bfd25892e5d1f327076956fae2d11bd07eb
esportleague <58951999+esportleague@users.noreply.github.com>
2021-01-21 19:20:08 +0100

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -10,27 +10,27 @@ export default ({
 }) => (
   <div>
     <Divider />
-    <Segment clearing size="large">
-      <span>
-        <strong>Sub total:</strong>
-        {` ${formatted}`}
-      </span>
-      <StripeCheckout
-        name="Gatsby Store"
-        amount={amount}
-        currency={currency || 'GBP'}
-        stripeKey={process.env.STRIPE_PUBLISHABLE_KEY || ''}
-        shippingAddress={false}
-        billingAddress
-        zipCode
-        token={handleCheckout}
-        reconfigureOnUpdate={false}
-        triggerEvent="onClick"
-      >
-        <Button color="black" floated="right">
-          Check out
-        </Button>
-      </StripeCheckout>
-    </Segment>
+//     <Segment clearing size="large">
+//       <span>
+//         <strong>Sub total:</strong>
+//         {` ${formatted}`}
+//       </span>
+//       <StripeCheckout
+//         name="Gatsby Store"
+//         amount={amount}
+//         currency={currency || 'GBP'}
+//         stripeKey={process.env.STRIPE_PUBLISHABLE_KEY || ''}
+//         shippingAddress={false}
+//         billingAddress
+//         zipCode
+//         token={handleCheckout}
+//         reconfigureOnUpdate={false}
+//         triggerEvent="onClick"
+//       >
+//         <Button color="black" floated="right">
+//           Check out
+//         </Button>
+//       </StripeCheckout>
+//     </Segment>
   </div>
 )
